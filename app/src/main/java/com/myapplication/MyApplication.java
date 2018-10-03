2b3de311626722a6c3b68d405b12494690bc5074
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-03 10:50:44 +0330

    Implement ExceptionHandler

--- app/src/main/java/com/myapplication/MyApplication.java
+++ app/src/main/java/com/myapplication/MyApplication.java
@@ -13,6 +13,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Thread.setDefaultUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
         sAnalytics = GoogleAnalytics.getInstance(this);
     }
 
