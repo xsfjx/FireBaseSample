435fe685e02e96b45a96c6ff1e522e27a1559340
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-10-03 09:30:41 +0330

    Implemented a tracker

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -1,15 +1,41 @@
 package com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Tracker tracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = findViewById(R.id.btn);
+        tracker = MyApplication.getDefaultTracker();
 
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tracker.send(new HitBuilders.EventBuilder()
+                        .setCategory("Action")
+                        .setAction("Share")
+                        .build());
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tracker.setScreenName("Main : onResume");
+        tracker.send(new HitBuilders.ScreenViewBuilder().build());
     }
 }
